323ab1ea81a1fdbd038208bfd9840ac0cd5b50bc
solidados <geekbrains.pk@internet.ru>
2024-10-10 15:42:25 +0400

    chore: update messages.js

--- src/helpers/messages.js
+++ src/helpers/messages.js
@@ -1,8 +1,13 @@
 import { EOL } from "node:os";
 
 const messages = {
+  goodbye: (username) =>
+    console.log(
+      `${EOL}Thank you for using File Manager, ${username}, goodbye!`,
+    ),
   greeting: (username) =>
     console.log(`${EOL}Welcome to the File Manager, ${username}!`),
+  invalid: () => console.log(`Invalid command${EOL}`),
   location: (dir) => console.log(`You are currently in ${dir}${EOL}`),
 };
 
