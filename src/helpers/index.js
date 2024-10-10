cee3a94fb456472d3b7aea98c5e9ce970b48c4cb
solidados <geekbrains.pk@internet.ru>
2024-10-10 16:44:48 +0400

    chore: add absolute identifier to pathsHelper.js and update helpers barrel exports

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,2 +1,2 @@
 export * from "./pathsHelper.js";
-export { messages } from "./messages.js";
+export messages from "./messages.js";
--- src/helpers/pathsHelper.js
+++ src/helpers/pathsHelper.js
@@ -1,5 +1,7 @@
-import { fileURLToPath } from "url";
-import { dirname } from "path";
+import { fileURLToPath } from "node:url";
+import { dirname, join, isAbsolute } from "node:path";
 
 export const __filename = fileURLToPath(import.meta.url);
 export const __dirname = dirname(__filename);
+export const __absolute = (dir, ...file) =>
+  isAbsolute(join(...file)) ? join(...file) : join(dir, join(...file));
