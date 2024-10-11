79a0fa50b027e49c656df2e3b3182ebd2e298c92
solidados <geekbrains.pk@internet.ru>
2024-10-11 22:56:25 +0400

    refactor: delete comments from `rn.command.js`

--- src/commands/rn.command.js
+++ src/commands/rn.command.js
@@ -1,9 +1,6 @@
-import { homedir as getHomeDir } from "node:os";
 import { rename } from "node:fs/promises";
 import { parse } from "node:path";
 import { __absolute, isPathExist, messages } from "../helpers/index.js";
-
-// let currentDir = getHomeDir();
 
 const rn = async (dir, filePath, newFileName) => {
   if (!filePath || !newFileName) {
@@ -21,30 +18,7 @@ const rn = async (dir, filePath, newFileName) => {
     }
 
     await rename(absoluteOldPath, absoluteNewPath);
-    // if (!filePath || !file) {
-    //   messages.invalidInput();
-    //   return dir;
-    // }
-
-    // let newPath = parse(filePath).dir;
-    // let oldPath = parse(filePath).base;
-
-    // try {
-    //   await isPathExist(__absolute(dir, newPath, oldPath));
-    // } catch (error) {
-    //   messages.failed(error.message);
-    //   return dir;
-    // }
-    // await rename(
-    //   __absolute(dir, newPath, oldPath),
-    //   __absolute(dir, newPath, file),
-    // );
     messages.fileRenamed(newFileName);
-    //
-    // currentDir = __absolute(dir, newPath);
-    // messages.location(currentDir);
-
-    // return currentDir;
   } catch (error) {
     messages.failed(error.message);
     return;
