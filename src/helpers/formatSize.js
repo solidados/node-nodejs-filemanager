97a00b574c30bb777272e498b06639d657e637bc
solidados <geekbrains.pk@internet.ru>
2024-10-13 22:03:21 +0400

    refactor: update formatSize.js

--- src/helpers/formatSize.js
+++ src/helpers/formatSize.js
@@ -1,6 +1,8 @@
 const formatSize = (size) => {
   const units = ["B", "KB", "MB", "GB", "TB"];
   const i = Math.floor(Math.log(size) / Math.log(1024));
+
+  if (size === 0) return "0 bytes";
   return `${(size / Math.pow(1024, i)).toFixed(2) * 1} ${units[i]}`;
 };
 
