858786982b0288d66fa723ff59f229054b0f7142
solidados <geekbrains.pk@internet.ru>
2024-10-14 12:28:07 +0400

    refactor: update compress - add compressRatio indicator

--- src/commands/compress.command.js
+++ src/commands/compress.command.js
@@ -1,4 +1,5 @@
 import { createReadStream, createWriteStream } from "node:fs";
+import { stat } from "node:fs/promises";
 import { parse } from "node:path";
 import { pipeline } from "node:stream/promises";
 import { createBrotliCompress } from "node:zlib";
@@ -11,16 +12,29 @@ const compress = async (dir, filePath, destPath) => {
     const source = __absolute(dir, filePath);
     const target = __absolute(dir, destPath);
     const fileWithExt = parse(source).base;
+    const compressedFile = `${target}_zip.br`;
 
     if (!(await isPathExist(source))) return messages.invalidInput();
 
+    const { size: originalSize } = await stat(source);
+
     const readStream = createReadStream(source);
-    const writeStream = createWriteStream(`${target}_compressed.br`);
+    const writeStream = createWriteStream(compressedFile);
     const brotli = createBrotliCompress();
 
     await pipeline(readStream, brotli, writeStream);
 
-    messages.fileProcessed("Compressed", fileWithExt);
+    const { size: compressedSize } = await stat(compressedFile);
+
+    const compressionRatio = (
+      ((originalSize - compressedSize) / originalSize) *
+      100
+    ).toFixed(2);
+
+    messages.fileProcessed(
+      `Compressed (Ratio: ${compressionRatio}%)`,
+      fileWithExt,
+    );
     messages.location(dir);
   } catch (error) {
     messages.failed(error.message);
