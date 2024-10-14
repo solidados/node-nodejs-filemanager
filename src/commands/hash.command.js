b4966a77040bc45ff3955d2dd60113b4b0440505
solidados <geekbrains.pk@internet.ru>
2024-10-14 23:32:23 +0400

    refactor: add pathParser.js to hash.command.js

--- src/commands/hash.command.js
+++ src/commands/hash.command.js
@@ -1,17 +1,20 @@
 import { readFile } from "node:fs/promises";
 import * as crypto from "node:crypto";
-import { __absolute, messages } from "../helpers/index.js";
+import { __absolute, messages, pathParser } from "../helpers/index.js";
 
 const hash = async (dir, file) => {
-  if (!file) return messages.invalidInput();
+  const [parsedDir] = pathParser(dir);
+  const [parsedFile] = pathParser(file);
+
+  if (!parsedFile) return messages.invalidInput();
 
   try {
-    const filePath = __absolute(dir, file);
+    const filePath = __absolute(parsedDir, parsedFile);
     const data = await readFile(filePath);
     const hash = crypto.createHash("sha256").update(data).digest("hex");
 
     messages.userOutput(hash);
-    messages.location(dir);
+    messages.location(parsedDir);
   } catch (error) {
     messages.failed(error.message);
   }
