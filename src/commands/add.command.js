437542e538b2fccfa5ef94fcd3fd92fc4138a1aa
solidados <geekbrains.pk@internet.ru>
2024-10-14 22:56:05 +0400

    refactor: add pathParser.js to add.command.js

--- src/commands/add.command.js
+++ src/commands/add.command.js
@@ -1,19 +1,22 @@
 import { readdir, writeFile } from "node:fs/promises";
-import { __absolute, messages } from "../helpers/index.js";
+import { __absolute, messages, pathParser } from "../helpers/index.js";
 
 const add = async (dir, file) => {
-  if (!file) return messages.invalidInput();
+  const [parsedDir] = pathParser(dir);
+  const [parsedFile] = pathParser(file);
+
+  if (!parsedFile) return messages.invalidInput();
 
   try {
-    const dirContent = await readdir(dir);
-    if (dirContent.includes(file)) return messages.fileExist();
+    const dirContent = await readdir(parsedDir);
+    if (dirContent.includes(parsedFile)) return messages.fileExist();
 
-    await writeFile(__absolute(dir, file), "");
-    messages.fileCreated(file);
+    await writeFile(__absolute(parsedDir, parsedFile), "");
+    messages.fileCreated(parsedFile);
   } catch (error) {
     messages.failed(error.message);
   } finally {
-    messages.location(dir);
+    messages.location(parsedDir);
   }
 };
 
