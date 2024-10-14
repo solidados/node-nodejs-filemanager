b246de87a717cfed4867adfe439a8c9546218942
solidados <geekbrains.pk@internet.ru>
2024-10-14 10:43:39 +0400

    refactor: update pathsHelper.js with resolve for absolute paths

--- src/helpers/pathsHelper.js
+++ src/helpers/pathsHelper.js
@@ -1,7 +1,9 @@
 import { fileURLToPath } from "node:url";
-import { dirname, join, isAbsolute } from "node:path";
+import { dirname, join, isAbsolute, resolve } from "node:path";
 
 export const __filename = fileURLToPath(import.meta.url);
 export const __dirname = dirname(__filename);
-export const __absolute = (dir, ...file) =>
-  isAbsolute(join(...file)) ? join(...file) : join(dir, join(...file));
+export const __absolute = (dir, ...filePath) => {
+  const fullPath = join(...filePath);
+  return isAbsolute(fullPath) ? fullPath : resolve(dir, fullPath);
+};
