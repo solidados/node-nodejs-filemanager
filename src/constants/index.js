8a88a371f7c03dc97418bbf095bfdb0d90affe47
solidados <geekbrains.pk@internet.ru>
2024-10-14 15:33:53 +0400

    chore: start add colors to application

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,4 +3,17 @@ import { homedir } from "node:os";
 export const constants = {
   username: "Guest",
   homedir: homedir(),
+  colors: {
+    blue: "\x1b[34m",
+    cyan: "\x1b[36m",
+    gray: "\x1b[90m",
+    green: "\x1b[32m",
+    purple: "\x1b[35m",
+    red: "\x1b[31m",
+    redLight: "\x1b[91m",
+    reset: "\x1b[0m",
+    white: "\x1b[37m",
+    whiteBg: "\x1b[7m",
+    yellow: "\x1b[33m",
+  },
 };
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,3 +3,10 @@ export { default as messages } from "./messages.js";
 export { default as isPathExist } from "./isPathExist.js";
 export { default as formatSize } from "./formatSize.js";
 export { default as fileOperationsHandler } from "./fileOperationsHandler.js";
+export {
+  formatInverseColor,
+  formatPathColor,
+  formatSuccessColor,
+  formatWarningColor,
+  formatErrorColor,
+} from "./formatTextColor.js";
